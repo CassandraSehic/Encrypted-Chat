dd3d4e0c03b2d8659d4d80ceb7b7f993603818ed
CassandraSehic <95361457+CassandraSehic@users.noreply.github.com>
2023-01-06 17:48:32 -0500

    Delete module-info.java

--- chat/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module net.sehic.cassandra.chat {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens net.sehic.cassandra.chat to javafx.fxml;
-    exports net.sehic.cassandra.chat;
-}
